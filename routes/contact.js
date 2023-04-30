501f65972c867b864a102ddd89e5159911e3420d
Moon Moon <purnsup90@gmail.com>
2023-04-29 23:29:15 -0600

    w02

--- routes/contact.js
+++ routes/contact.js
@@ -2,6 +2,7 @@ import express from 'express';
 import * as contact from '../controllers/contact.js';
 
 const route = express.Router();
+
 route.get('/', contact.getAll);
 route.get('/:id', contact.getById);
 
